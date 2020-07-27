55be1db9ba6d5d29637fc6fee2b895e35c848b5f
Bastien <bab.muller@yahoo.fr>
2020-07-27 18:05:35 +0200

    cache
    reconnect

--- public/socket.js
+++ public/socket.js
@@ -1,9 +1,20 @@
 var socket = io();
 
+const verify_user = () => {
+    let cache = getLocalStorage('emile');
+    if (!cache) return;
+    socket.emit('login', cache);
+    toWaitingRoom();
+};
+
+verify_user();
+
 const login = () => {
     let input = document.getElementById('username');
     let username = input ? input.value : 'user';
     let id = generate_id();
+
+    setLocalStorage('emile', {id, username});
 
     socket.emit('login', {id, username});
     toWaitingRoom();
--- public/utils.js
+++ public/utils.js
@@ -1,3 +1,16 @@
 const generate_id = () => Math.random().toString(36).substr(2, 9);
 
+// Retreive data from browser cache
+function getLocalStorage( tag ) {
+    if ( typeof( Storage ) !== "undefined" ) return JSON.parse( localStorage.getItem( tag ) );
+};
 
+// Save data to browser cache
+function setLocalStorage( tag, obj ) {
+    if ( typeof( Storage ) !== "undefined" ) localStorage.setItem( tag, JSON.stringify( obj ) );
+};
+
+// Delete data from browser cache
+function delLocalStorage( tag ) {
+    localStorage.removeItem( tag );
+};
